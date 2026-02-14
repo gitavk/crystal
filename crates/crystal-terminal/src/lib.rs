afce2efea01c607f979020ecc195426a59d0c340
gitavk <3apa3ka3@gmail.com>
2026-02-14 12:33:59 +0100

    ch05: Context-Aware Environment

--- crates/crystal-terminal/src/lib.rs
+++ crates/crystal-terminal/src/lib.rs
@@ -1,7 +1,9 @@
+mod context_env;
 mod pty;
 pub mod renderer;
 mod vt;
 
+pub use context_env::ContextEnv;
 pub use pty::PtySession;
 pub use renderer::render_terminal_screen;
 pub use vt::VtParser;
